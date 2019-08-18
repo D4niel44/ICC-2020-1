59070b41ec3eed6a60905c80ca7d82b586c5d0a8
Daniel <daniel@asus>
2019-08-17 22:15:35 -0500

    Práctica 1 y 2.

---
+++ practica01/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	System.out.println("openjdk version \"11.0.4\" 2019-07-16");
+	System.out.println("OpenJDK Runtime Environment (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3)");
+	System.out.println("OpenJDK 64-Bit Server VM (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3, mixed mode, sharing)\n");
+	System.out.println("Linux asus 4.9.0-9-amd64 #1 SMP Debian 4.9.168-1+deb9u4 (2019-07-19) x86_64 GNU/Linux");
+    }
+    
+}
