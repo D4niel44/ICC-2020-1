8fb0835e982bb345fb2deac68137e639a6dfbc22
D4niel44 <daniel.linares@ciencias.unam.mx>
2019-09-05 11:08:56 -0500

    Practica 06

---
+++ practica06/Temperatura.java
@@ -0,0 +1,60 @@
+
+
+public class Temperatura {
+
+    public static int CELSIUS = 2;
+    public static int FARENHEIT = 1;
+
+    double valor;
+    int escala;
+
+    public Temperatura(double valor, int escala) {
+
+	this.valor = valor;
+	this.escala = escala;
+    }
+
+    public double convertirACelsius(double valorF) {
+
+	return valorF * 1.8 + 32; 
+    }
+
+    public double convertirAFarenheit(double valorC) {
+
+	return (valorC - 32) / 1.8; 
+    }
+
+    public double obtenerTemperaturaenCelsius() {
+
+	if (escala == CELSIUS) {
+	    
+	    return valor;
+	}
+	else {
+	    
+	    return convertirACelsius(valor);
+	}
+    }
+
+    public double obtenerTemperaturaenFarenheit() {
+
+	if (escala == FARENHEIT) {
+
+	    return valor;
+	}
+	else {
+
+	    return convertirAFarenheit(valor);
+	}
+    }
+
+    public boolean esIgual(Temperatura temp) {
+
+	return = escala == temp.escala && valor == temp.valor;
+    }
+
+    public boolean esMayor(Temperatura temp) {
+
+	return obtenerTemperaturaenCelsius() > temp.obtenerTemperaturaenCelsius();
+    }
+}
